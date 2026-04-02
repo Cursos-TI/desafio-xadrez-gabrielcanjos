a118e7639f54b32f57c1f2acb77729b581663c60
Gabriel Anjos <gabrielalvares5000@gmail.com>
2026-04-02 15:48:46 +0000

    Nível Aventureiro: movimentação do cavalo com loops aninhados

--- xadrez.c
+++ xadrez.c
@@ -5,19 +5,17 @@ int main() {
     int i;
 
     // ======================
-    // TORRE (for)
+    // TORRE
     // ======================
     printf("Movimento da Torre:\n");
-
     for (i = 0; i < 5; i++) {
         printf("Direita\n");
     }
 
     // ======================
-    // BISPO (while)
+    // BISPO
     // ======================
     printf("\nMovimento do Bispo:\n");
-
     i = 0;
     while (i < 5) {
         printf("Cima Direita\n");
@@ -25,15 +23,31 @@ int main() {
     }
 
     // ======================
-    // RAINHA (do-while)
+    // RAINHA
     // ======================
     printf("\nMovimento da Rainha:\n");
-
     i = 0;
     do {
         printf("Esquerda\n");
         i++;
     } while (i < 8);
+
+    // ======================
+    // CAVALO (loops aninhados)
+    // ======================
+    printf("\nMovimento do Cavalo:\n");
+
+    int j;
+
+    for (i = 0; i < 2; i++) { // duas casas para baixo
+        printf("Baixo\n");
+    }
+
+    j = 0;
+    while (j < 1) { // uma casa para esquerda
+        printf("Esquerda\n");
+        j++;
+    }
 
     return 0;
 }
